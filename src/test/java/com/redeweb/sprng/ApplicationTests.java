cea0b3144e2ec3ed36dc69ae4f28ad523a52affe
Jonatasvm <109855262+Jonatasvm@users.noreply.github.com>
2025-12-27 15:07:17 -0400

    commit inicial

---
+++ src/test/java/com/redeweb/sprng/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redeweb.sprng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
