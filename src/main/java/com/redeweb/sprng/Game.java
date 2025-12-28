d2e4d70ea10d3f087b8f3ba493deb9d0c2915e6c
Jonatasvm <109855262+Jonatasvm@users.noreply.github.com>
2025-12-28 11:03:16 -0400

    propriedades novas para gerar o banco de dados

---
+++ src/main/java/com/redeweb/sprng/Game.java
@@ -0,0 +1,104 @@
+package com.redeweb.sprng;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_game")
+
+public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String title;
+
+    @Column(name = "game_year")
+    private Integer year;
+    private String genre;
+    private String plataform;
+    private String imgUrl;
+    private String shortDescription;
+    private String longDescription;
+
+    public Game(long id, String title, Integer year, String genre, String plataform, String imgUrl, String shortDescription, String longDescription) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.genre = genre;
+        this.plataform = plataform;
+        this.imgUrl = imgUrl;
+        this.shortDescription = shortDescription;
+        this.longDescription = longDescription;
+    }
+//geters
+    public long getId() {
+        return id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public Integer getYear() {
+        return year;
+    }
+    public String getGenre() {
+        return genre;
+    }
+    public String getPlataform() {
+        return plataform;
+    }
+    public String getImgUrl() {
+        return imgUrl;
+    }
+    public String getShortDescription() {
+        return shortDescription;
+    }
+    public String getLongDescription() {
+        return longDescription;
+    }
+//setters
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public void setPlataform(String plataform) {
+        this.plataform = plataform;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public void setLongDescription(String longDescription) {
+        this.longDescription = longDescription;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Game game)) return false;
+        return id == game.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
